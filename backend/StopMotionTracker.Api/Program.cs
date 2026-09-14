36cd7849cfc50b97f63836b2ff87fca288e589b7
Matilda Stokes <jl21579@bristol.ac.uk>
2026-09-14 21:18:11 +0100

    more SQLite adjustments

--- backend/StopMotionTracker.Api/Program.cs
+++ backend/StopMotionTracker.Api/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors(options =>
 {
