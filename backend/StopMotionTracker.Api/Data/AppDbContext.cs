1d61befcfa3fc559514f5d0f329be26d23d4178d
Matilda Stokes <matildarosevin@gmail.com>
2026-09-14 12:08:20 +0100

    Add comment to AppDbContext class definition
    Added comment to clarify the purpose of the AppDbContext class.

--- backend/StopMotionTracker.Api/Data/AppDbContext.cs
+++ backend/StopMotionTracker.Api/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using StopMotionTracker.Api.Models;
 
 namespace StopMotionTracker.Api.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : DbContext //initialise class Db
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
